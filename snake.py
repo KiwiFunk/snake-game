cd6862462b90b0a7a88cb33fc4daec8220183c5b
Alex <alex_p97@live.com>
2025-02-15 10:49:54 +0000

    Created game window and exit functionality

--- snake.py
+++ snake.py
@@ -1,1 +1,20 @@
 import pygame
+import sys
+
+#init pygame modules
+pygame.init()
+
+#Create a display surface/Game window
+screen = pygame.display.set_mode((500, 500))
+
+#GAME LOOP START
+while True:
+
+    #Listen for events
+    for event in pygame.event.get():
+        #If window is closed, ensure game and code execution is ended
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    #Update game display
+    pygame.display.update()
