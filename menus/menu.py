9f945879af558a9fe3cafb883116a1218c786de4
Alex <alex_p97@live.com>
2025-02-17 23:23:32 +0000

    Updated title screen graphics

--- menus/menu.py
+++ menus/menu.py
@@ -8,7 +8,7 @@ class Menu:
         self.screen = screen
         self.grid_size = grid_size
         self.cell_size = cell_size
-        self.font = pygame.font.Font('assets/fonts/sprint-2.ttf', 30)
+        self.font = pygame.font.Font('assets/fonts/LeoRoundedProMedium.ttf', 45)
         self.titlefont = pygame.font.Font('assets/fonts/sprint-2.ttf', 100)
 
     def draw_grass(self, screen):
@@ -25,15 +25,17 @@ class Menu:
 
         self.draw_grass(self.screen)
 
-        title_text = self.titlefont.render("Snake", True, (255, 255, 255))
-        title_rect = title_text.get_rect(center=(self.grid_size * self.cell_size // 2, self.grid_size * self.cell_size // 4))
-        self.screen.blit(title_text, title_rect)
 
-        start_text = self.font.render("Press Enter to Start", True, (255, 255, 255))
-        start_rect = start_text.get_rect(center=(self.grid_size * self.cell_size // 2, self.grid_size * self.cell_size // 2))
+        
+        title_image = pygame.image.load('assets/menugraphics/title.png')
+        title_rect = title_image.get_rect(center=(self.grid_size * self.cell_size // 2, self.grid_size * self.cell_size // 4))
+        self.screen.blit(title_image, title_rect)
+
+        start_text = self.font.render("Press enter to StArt", True, (255, 255, 255))
+        start_rect = start_text.get_rect(center=(self.grid_size * self.cell_size // 2, self.grid_size * self.cell_size // 1.8))
         self.screen.blit(start_text, start_rect)
 
-        quit_text = self.font.render("Press Esc to Quit", True, (255, 255, 255))
+        quit_text = self.font.render("PrEss esc to Quit", True, (255, 255, 255))
         quit_rect = quit_text.get_rect(center=(self.grid_size * self.cell_size // 2, self.grid_size * self.cell_size // 1.5))
         self.screen.blit(quit_text, quit_rect)
 
