7ede6e5323ab5e318d1c4d987b1e0b097fe18ed1
Alex <alex_p97@live.com>
2025-02-17 22:32:34 +0000

    Added draw_grass to menu.py, changed font sizing

--- menus/menu.py
+++ menus/menu.py
@@ -2,16 +2,30 @@
 import pygame
 import sys
 
+
 class Menu:
     def __init__(self, screen, grid_size, cell_size):
         self.screen = screen
         self.grid_size = grid_size
         self.cell_size = cell_size
-        self.font = pygame.font.Font('assets/fonts/sprint-2.ttf', 40)
+        self.font = pygame.font.Font('assets/fonts/sprint-2.ttf', 30)
+        self.titlefont = pygame.font.Font('assets/fonts/sprint-2.ttf', 100)
+
+    def draw_grass(self, screen):
+        grass_color = (170, 215, 81)
+        grass_dark = (160, 208, 72)
+        for row in range(self.grid_size):
+            for col in range(self.grid_size):
+                grass_rect = pygame.Rect(col * self.cell_size, row * self.cell_size, self.cell_size, self.cell_size)
+                color = grass_color if (row + col) % 2 == 0 else grass_dark
+                pygame.draw.rect(screen, color, grass_rect)
 
     def draw(self):
-        self.screen.fill((0, 0, 0))  # black background
-        title_text = self.font.render("Snake Game", True, (255, 255, 255))
+        #self.screen.fill((0, 0, 0))  # black background
+
+        self.draw_grass(self.screen)
+
+        title_text = self.titlefont.render("Snake", True, (255, 255, 255))
         title_rect = title_text.get_rect(center=(self.grid_size * self.cell_size // 2, self.grid_size * self.cell_size // 4))
         self.screen.blit(title_text, title_rect)
 
